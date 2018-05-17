66a82d52ece31d3fbcff64bff6ae7d465961ddd1
IslyambekKairov <kairov2605@gmail.com>
2018-05-17 10:02:34 +0600

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Add(2, 3);
+            Console.ReadKey();
+        }
+    }
+
+    // объявление нового класса
+    class Calculator
+    {
+        public static void Add(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine($"Сумма {x} и {y} равна {z}");
+        }
+    }
+}
